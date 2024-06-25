1bbc625dd7f8c6ba7716bf89c3903574bec2d15c
João Campista <joaocampista.jc@gmail.com>
2024-06-25 20:18:10 -0300

    adicionado novo lambda

--- lambda.tf
+++ lambda.tf
@@ -14,3 +14,20 @@ module lambda-teste {
       WORKSPACE = "teste",
     }
 }
+
+module lambda-teste2 {
+    source = "./modules/lambda"
+    lambda_repo = "git@github.com:dionakra-ai/lambda-teste2.git"
+    description = "Lambda de testes 2"
+    function_name = "lambda-teste2"
+    runtime = "python3.10"
+    handler = "main.lambda_handler"
+    iam_prefix = "lambda_exec_"
+    memory_size = 128
+    timeout = 30
+    no_build = true
+    no_clone = true
+    venvs = {
+      WORKSPACE = "teste",
+    }
+}
